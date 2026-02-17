fc7afee830951c7ec105cdc8309425e448976a59
Jeerasak09 <jeerasak_Kunnoi@cmu.ac.th>
2026-02-17 23:03:21 +0700

    Source Code Ugame_3.cpp

--- Source Code Ugame.cpp
+++ Source Code Ugame.cpp
@@ -6,23 +6,50 @@
 
 using namespace std;
 
-class Student {
+class Student{
     public:
     string name;
     string jobClass;
+    int hp;
+    int maxHp;
     int stamina;
     int maxStamina;
     int power;
+    int defense;
     int agility;
 
-    Student(string n, string job, int hp, int atk, int agi) {
+    Student(string n, string job, int h, int stam, int atk, int def, int agi){
         name = n;
         jobClass = job;
-        stamina = hp;
-        maxStamina = hp;
+        maxHp = h;
+        hp = h;
+        maxStamina = stam;
+        stamina = stam;
         power = atk;
+        defense = def;
         agility = agi;
     }
+
+    bool isKnockedOut(){
+        return hp <= 0;
+    }
+
+    void takeHit(int dmg){
+        hp -= dmg;
+        if (hp < 0) hp = 0;
+    }
+
+    void recoverFull(){
+        hp = maxHp;
+        stamina = maxStamina;
+    }
+
+
+
+
+
+
+    
     
 
 
